8defbda838ba8c2bb6bdd50626600cea3ee57d63
Kate Halushka <katehalushka@gmail.com>
2023-12-01 19:09:12 -0500

    Clean up old auth

--- src/App.js
+++ src/App.js
@@ -1,123 +1,87 @@
 import './App.css';
+import {useEffect, useState} from 'react';
 
 const clientId = "5ce1937c472e49ff980b2daf69f969cc"; // Replace with your client ID
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
-var profile;
-var songs;
+//var profile;
+// var songs;
 
-if (!code) {
-  redirectToAuthCodeFlow(clientId);
-} else {
-  const accessToken = await getAccessToken(clientId, code);
-  console.log(accessToken);
-  profile = await fetchProfile(accessToken);
-  console.log(profile);
-  const playlists = await fetchPlaylists(accessToken);
-  console.log(playlists);
-  songs = await getDiff(playlists, accessToken);
-  console.log(songs);
-
-}
+// if (!code) {
+//   redirectToAuthCodeFlow(clientId);
+// } else {
+//   const accessToken = await getAccessToken(clientId, code);
+//   console.log(accessToken);
+//   profile = await fetchProfile(accessToken);
+//   console.log(profile);
+//   const playlists = await fetchPlaylists(accessToken);
+//   console.log(playlists);
+//   songs = await getDiff(playlists, accessToken);
+//   console.log(songs);
+// }
 
 function App() {
+  const CLIENT_ID = "5ce1937c472e49ff980b2daf69f969cc"
+  const REDIRECT_URI = "https://year-over-year.web.app/"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+  const [token, setToken] = useState("")
+  const [profile, setProfile] = useState({})
+  const [songs, setSongs] = useState([])
+
+  useEffect(async () => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+    const result = await fetch("https://api.spotify.com/v1/me", {
+      method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    setProfile(await result.json());
+    const playlists = await fetchPlaylists(token);
+    setSongs(await getDiff(playlists, token));
+
+  }, [])
+
   const renderSongs = () => {
-    console.log('getting here');
     return songs.map(element => (
       <div key={element}>
         {element}
       </div>
     ))
-    // songs.forEach(element => {
-    //   <div>
-    //     Hello!
-    //     {element}
-    //   </div>
-    // });
-    // return songs.map(artist => (
-    //     <div key={artist.id}>
-    //         {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-    //         {artist.name}
-    //     </div>
-    // ))
   }
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+    setSongs([])
+    setProfile({})
+  }
+
   return (
     <div className="App">
     <header className="App-header">
-        <h1>Spotify React</h1>
-        <p>{profile.display_name}</p>
-        {renderSongs()}
+        <h1>Year Over Year</h1>
+        <p>See what songs made your top 100 songs playlists in both 2022 and 2023!</p>
+        {!token ?
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-private,user-read-email,playlist-read-private,playlist-read-collaborative`}>Login to Spotify</a>
+        : <p>Profile: {profile.display_name}</p>}
+        <ul>{renderSongs()}</ul>
+        {/* {profile ? renderSongs() : <p></p>} */}
+        <button onClick={logout}>Logout</button>
         
     </header>
 </div>
   );
-}
-
-async function redirectToAuthCodeFlow(clientId) {
-  // TODO: Redirect to Spotify authorization page
-  const verifier = generateCodeVerifier(128);
-  const challenge = await generateCodeChallenge(verifier);
-
-  localStorage.setItem("verifier", verifier);
-
-  const params = new URLSearchParams();
-  params.append("client_id", clientId);
-  params.append("response_type", "code");
-  params.append("redirect_uri", "https://year-over-year.web.app/");
-  params.append("scope", "user-read-private user-read-email playlist-read-private");
-  params.append("code_challenge_method", "S256");
-  params.append("code_challenge", challenge);
-
-  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-}
-
-function generateCodeVerifier(length) {
-  let text = '';
-  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-async function generateCodeChallenge(codeVerifier) {
-  const data = new TextEncoder().encode(codeVerifier);
-  const digest = await window.crypto.subtle.digest('SHA-256', data);
-  return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .replace(/=+$/, '');
-}
-
-async function getAccessToken(clientId, code) {
-  // TODO: Get access token for code
-  const verifier = localStorage.getItem("verifier");
-
-  const params = new URLSearchParams();
-  params.append("client_id", clientId);
-  params.append("grant_type", "authorization_code");
-  params.append("code", code);
-  params.append("redirect_uri", "https://year-over-year.web.app/");
-  params.append("code_verifier", verifier);
-
-  const result = await fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: params
-  });
-
-  const { access_token } = await result.json();
-  return access_token;
-}
-
-async function fetchProfile(token) {
-  // TODO: Call Web API
-  const result = await fetch("https://api.spotify.com/v1/me", {
-      method: "GET", headers: { Authorization: `Bearer ${token}` }
-  });
-
-  return await result.json();
 }
 
 async function fetchPlaylists(token) {
@@ -183,6 +147,8 @@ async function getDiff(playlists, token){
       songs_overlap.push(`${element.track.name} - ${element.track.artists[0].name}`);
     }
   });
+  console.log("here");
+  console.log(songs_overlap);
 
   return songs_overlap;
 
