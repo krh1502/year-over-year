e0b50fd8909f359d85ccf16a8000dee0de0462ef
Kate Halushka <62454384+krh1502@users.noreply.github.com>
2024-12-06 14:06:37 -0500

    Bugfix/nullplaylist (#9)
    * bugfix
    
    * i hate spotify
    
    * design updates etc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const CLIENT_ID = "5ce1937c472e49ff980b2daf69f969cc"
-  const REDIRECT_URI = "https://year-over-year.web.app/"
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
   const [token, setToken] = useState("")
@@ -13,30 +13,29 @@ function App() {
   const [selectedYears, setSelectedYears] = useState([])
 
   useEffect(() => {
-    
+
     const hash = window.location.hash
     let token = window.localStorage.getItem("token")
-   //console.log("here: "+token)
-    if(!token){
-      window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-private,user-read-email,playlist-read-private,playlist-read-collaborative`
-    }
+    // if (!token) {
+    //   window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-private,user-read-email,playlist-read-private,playlist-read-collaborative`
+    // }
 
     if (!token && hash) {
-        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-        window.location.hash = ""
-        window.localStorage.setItem("token", token)
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
     }
 
     setToken(token)
-    async function fetchData(){
+    async function fetchData() {
       const result = await fetch("https://api.spotify.com/v1/me", {
         method: "GET", headers: { Authorization: `Bearer ${token}` }
       });
 
       setProfile(await result.json());
       setYears(await fetchYears(token))
-      
+
     }
 
     fetchData();
@@ -44,9 +43,17 @@ function App() {
   }, [])
 
   const renderSongs = () => {
+    if (songs[0] === 1) {
+      return (
+        <div>
+          <p class="song">No overlapping songs between these selected years.</p>
+        </div>
+      )
+
+    }
     return songs.map(element => (
       <div key={element}>
-        {element}
+        <a class="song" href={element.external_urls.spotify}>{element.name} - {element.artists[0].name}</a>
       </div>
     ))
   }
@@ -78,86 +85,102 @@ function App() {
     })
   };
 
-  const updatePlaylists = async () =>{
-    console.log(selectedYears)
+  const updatePlaylists = async () => {
     setSongs(await getDiffOptions(selectedYears, token))
   }
 
   return (
     <div className="App">
-    <header className="App-header">
+      <header className="App-header">
         <h1>Year Over Year</h1>
-        <p>See what songs made your top 100 songs playlists from Spotify Wrapped, year after year!</p>
+        <p class="textarea">See what songs made your top 100 songs playlists from Spotify Wrapped, year after year!</p>
+        <p class="textarea">How to use this app: <br />Add your Spotify wrapped top songs playlists to your library as a copy by clicking on the ..., then "Add to other playlist", then "New playlist". Keep the playlist name the same as it was (remove any extra numbers)</p>
         {!token || token === "" ?
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-private,user-read-email,playlist-read-private,playlist-read-collaborative`}>Login to Spotify</a>
-        : <p>Profile: {profile.display_name}</p>}
+          <div>
+            <a class="button-1" href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-private,user-read-email,playlist-read-private,playlist-read-collaborative`}>Login to Spotify</a>
+          </div>
+          : <div class="loggedin">
+            <p class="profile">Logged in as {profile.display_name}</p>
+            <button class="button-1" onClick={logout}>Logout</button>
+          </div>}
 
         {!years || Object.keys(years).length === 0 ?
           <p>No years available.</p> :
           <div>
-          {Object.entries(years).map(([key, value]) => (
-            <div key={value}>
-              <label>
-                <input
-                  type="checkbox"
-                  value={value}
-                  checked={selectedYears.includes(value)}
-                  onChange={handleCheckboxChange}
-                />
-                {key} {/* Assuming value is a display name */}
-              </label>
-            </div>
-          ))}
-          <button onClick={updatePlaylists}>Submit</button>
+            {Object.entries(years).map(([key, value]) => (
+              <div key={value} class="checkbox-wrapper-33">
+                <label class="checkbox">
+                  <input
+                    type="checkbox"
+                    value={value}
+                    checked={selectedYears.includes(value)}
+                    onChange={handleCheckboxChange}
+                    class="checkbox__trigger visuallyhidden"
+                  />
+                  <span class="checkbox__symbol">
+                    <svg aria-hidden="true" class="icon-checkbox" width="28px" height="28px" viewBox="0 0 28 28" version="1" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M4 14l8 7L24 7"></path>
+                    </svg>
+                  </span>
+                  <p class="checkbox__textwrapper">{key}</p> {/* Assuming value is a display name */}
+                </label>
+              </div>
+            ))}
+            {selectedYears.length === 2 ?
+              <button class="button-1" onClick={updatePlaylists} disabled={false}>Submit</button> :
+              <button class="button-1" onClick={updatePlaylists} disabled={true}>Submit</button>
+            }
           </div>
         }
 
-        
-        {token && !songs ?
-          <ul>
-            <p>Looks like you're missing your 2023 and/or 2022 Top Songs Playlist(s). Add them to your library:</p>
-            <p><a href="https://open.spotify.com/playlist/37i9dQZF1Fa1IIVtEpGUcU?si=8a8b251ae72546fb">2023 Playlist</a></p>
-            <p><a href="https://open.spotify.com/playlist/37i9dQZF1F0sijgNaJdgit?si=d2dce6c5e8c94892">2022 Playlist</a></p>
-          </ul> : 
-          <ul>{renderSongs()}</ul>
+
+        {token && songs.length !== 0 ?
+          <div class="songarea">{renderSongs()}</div> :
+          <div></div>
         }
-        <button onClick={logout}>Logout</button>
-        
-    </header>
-</div>
+
+
+
+
+      </header>
+      <footer class="App-footer">
+        <p>Data from</p>
+        <img class="spotify-logo" src='Full_Logo_White_RGB.svg' alt="Spotify logo"></img>
+
+      </footer>
+    </div>
   );
 }
 
 async function fetchYears(token) {
-  if(!token || token===""){
+  if (!token || token === "") {
     return {}
   }
   var result = await fetch("https://api.spotify.com/v1/me/playlists?limit=50&offset=0", {
-    method: "GET", headers: {Authorization: `Bearer ${token}`}
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
   });
 
   var res = await result.json();
-  console.log(res.items)
   let years = {}
-  for(let i=0;i<res.items.length;i++){
-    if(res.items[i] == null){
+  for (let i = 0; i < res.items.length; i++) {
+    if (res.items[i] == null) {
       continue
     }
-    if(res.items[i].name.startsWith("Your Top Songs")){
+    if (res.items[i].name.startsWith("Your Top Songs")) {
       years[res.items[i].name] = res.items[i].id
     }
   }
 
-  while(res.next) {
+  while (res.next) {
     result = await fetch(res.next, {
-      method: "GET", headers: {Authorization: `Bearer ${token}`}
+      method: "GET", headers: { Authorization: `Bearer ${token}` }
     });
     res = await result.json();
-    for(let i=0;i<res.items.length;i++){
-      if(res.items[i] == null){
+    for (let i = 0; i < res.items.length; i++) {
+      if (res.items[i] == null) {
         continue
       }
-      if(res.items[i].name.startsWith("Your Top Songs")){
+      if (res.items[i].name.startsWith("Your Top Songs")) {
         years[res.items[i].name] = res.items[i].id
       }
     }
@@ -165,71 +188,11 @@ async function fetchYears(token) {
   return years
 }
 
-// async function fetchPlaylists(token) {
-//   var result = await fetch("https://api.spotify.com/v1/me/playlists?limit=50&offset=0", {
-//     method: "GET", headers: {Authorization: `Bearer ${token}`}
-//   });
-
-//   var res = await result.json();
-//   let playlists = {};
-//   for(let i=0;i<res.items.length;i++){
-//     playlists[res.items[i].name] = res.items[i].id
-//   }
-
-//   if(playlists["Your Top Songs 2023"] && playlists["Your Top Songs 2022"]){
-//     return {'2023': playlists["Your Top Songs 2023"], '2022': playlists["Your Top Songs 2022"]}
-//   }
-
-//   while(res.next) {
-//     console.log(res.next)
-//     result = await fetch(res.next, {
-//       method: "GET", headers: {Authorization: `Bearer ${token}`}
-//     });
-//     res = await result.json();
-//     for(let i=0;i<res.items.length;i++){
-//       playlists[res.items[i].name] = res.items[i].id
-//     }
-//     if(playlists["Your Top Songs 2023"] && playlists["Your Top Songs 2022"]){
-//       return {'2023': playlists["Your Top Songs 2023"], '2022': playlists["Your Top Songs 2022"]}
-//     }
-//   }
-//   console.log("Missing 2023 and/or 2022 top songs playlist(s). Make sure to save these to your Spotify library.")
-//   return {'2023': null, '2022': null}
-// }
-
-// async function getDiff(playlists, token){
-//   var url = `https://api.spotify.com/v1/playlists/${playlists['2023']}/tracks?fields=items(track(name,artists(name)))`
-//   var songs = []
-//   var result = await fetch(url, {
-//     method: "GET", headers: {Authorization: `Bearer ${token}`}
-//   });
-
-//   var response = await result.json();
-//   response.items.forEach(element => {
-//     songs.push(`${element.track.name} - ${element.track.artists[0].name}`)
-//   });
-
-//   url = `https://api.spotify.com/v1/playlists/${playlists['2022']}/tracks?fields=items(track(name,artists(name)))`
-//   var songs_overlap = []
-//   result = await fetch(url, {
-//     method: "GET", headers: {Authorization: `Bearer ${token}`}
-//   });
-//   response = await result.json();
-//   response.items.forEach(element => {
-//     if(songs.includes(`${element.track.name} - ${element.track.artists[0].name}`)){
-//       songs_overlap.push(`${element.track.name} - ${element.track.artists[0].name}`);
-//     }
-//   });
-
-//   return songs_overlap;
-
-// }
-
-async function getDiffOptions(playlists, token){
-  var url = `https://api.spotify.com/v1/playlists/${playlists[0]}/tracks?fields=items(track(name,artists(name)))`
+async function getDiffOptions(playlists, token) {
+  var url = `https://api.spotify.com/v1/playlists/${playlists[0]}/tracks?fields=items(track(name,artists(name),href))`
   var songs = []
   var result = await fetch(url, {
-    method: "GET", headers: {Authorization: `Bearer ${token}`}
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
   });
 
   var response = await result.json();
@@ -237,17 +200,21 @@ async function getDiffOptions(playlists, token){
     songs.push(`${element.track.name} - ${element.track.artists[0].name}`)
   });
 
-  url = `https://api.spotify.com/v1/playlists/${playlists[1]}/tracks?fields=items(track(name,artists(name)))`
+  url = `https://api.spotify.com/v1/playlists/${playlists[1]}/tracks?fields=items(track(name,artists(name),external_urls(spotify)))`
   var songs_overlap = []
   result = await fetch(url, {
-    method: "GET", headers: {Authorization: `Bearer ${token}`}
+    method: "GET", headers: { Authorization: `Bearer ${token}` }
   });
   response = await result.json();
   response.items.forEach(element => {
-    if(songs.includes(`${element.track.name} - ${element.track.artists[0].name}`)){
-      songs_overlap.push(`${element.track.name} - ${element.track.artists[0].name}`);
+    if (songs.includes(`${element.track.name} - ${element.track.artists[0].name}`)) {
+      songs_overlap.push(element.track);
     }
   });
+
+  if (songs_overlap.length === 0) {
+    songs_overlap.push(1)
+  }
 
   return songs_overlap;
 
